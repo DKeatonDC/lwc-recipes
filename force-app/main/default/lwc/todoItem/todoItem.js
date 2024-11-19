49268ed70ac77a7736a7dce64ca1f113c8319637
DKeatonDC <165526945+DKeatonDC@users.noreply.github.com>
2024-11-19 18:59:32 -0500

    reflects changes to make component reactive

--- force-app/main/default/lwc/todoItem/todoItem.js
+++ force-app/main/default/lwc/todoItem/todoItem.js
@@ -3,7 +3,7 @@ import { LightningElement, api } from 'lwc';
 export default class TodoItem extends LightningElement {
     _uppercaseItemName;
     itemName="betty";
-
+    handleText(event){this.itemName=event.target.value}
     @api 
     get itemName(){
         return this._uppercaseItemName;
