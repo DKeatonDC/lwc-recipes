fc7e3820f06c4838158c84226f7cb4db1a8d2a14
DKeatonDC <165526945+DKeatonDC@users.noreply.github.com>
2024-12-01 06:45:23 -0500

    reflects commonts

--- force-app/main/default/lwc/todoItem/todoItem.js
+++ force-app/main/default/lwc/todoItem/todoItem.js
@@ -2,9 +2,17 @@ import { LightningElement, api } from 'lwc';
 
 export default class TodoItem extends LightningElement {
     _uppercaseItemName;
-    itemname="betty";
+    itemname="set by child";
+    //this will not appear in parent component because 
+    //the parent reassigns the value
     @api
     handleText(event){this.itemname=event.target.value}
+    //this function must be called by the parent. Changing
+    //the value of {itemname} on the parent alone e.g.
+    // <c-todo-item itemname={foo}> will not change the 
+    //value of itemname on the child.  I believe that this
+    // is because of the getter/setter conflicting
+    //with the parent
     @api 
     get itemname(){
         return this._uppercaseItemName;
@@ -12,6 +20,11 @@ export default class TodoItem extends LightningElement {
     set itemname(value){
         this._uppercaseItemName = value.toUpperCase();
     }
+    //the above allows things to appear uppercase including 
+    //set by child
     @api 
-    parenttext="placeholder";
+    parenttext = null;
+    if(parenttext = null){this.parenttext = "set by a child";}
+    //this will not appear in parent component because 
+    //the parent reassigns the value unless set by child
 }
--- force-app/main/default/lwc/todoParent/todoParent.js
+++ force-app/main/default/lwc/todoParent/todoParent.js
@@ -5,7 +5,10 @@ export default class TodoParent extends LightningElement {
     handleText(event){
 this.textValue = event.target.value;
     }
+    //this sets the value for parenttext on the child
     handleChildText(event){
         this.template.querySelector('c-todo-item').handleText(event);
     }
+    //this calls handleText on the child.  Note that that
+    //is a different function than the one on this page
 }
